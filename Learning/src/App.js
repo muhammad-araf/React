45caf7ef9873d1c11c6ddca58df6b6208af25f4f
muhammad-araf <lentoaraf@gmail.com>
2025-07-06 12:26:20 +0500

    Change Name with UseState and Props

--- Learning/src/App.js
+++ Learning/src/App.js
@@ -4,16 +4,17 @@ import ButtonEvent from './ButtonEvent';
 import StateIn from './StateIn';
 import CounterApp from './CounterApp';
 import PropsIn from './PropsIn';
-
+import StateAndProps from './StateAndProps';
+import React,{useState} from 'react';
+//  {useState} from 'react';
 function App() {
+  const [name,setName] = useState("Araf");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <h1>Araf</h1> */}
-        <PropsIn name={"Araf"}/>
-
-
+        <StateAndProps name={name}/>
+        <button onClick={() => setName("Muhammad")}>Change Name</button>
 
       </header>
     </div>
---
+++ Learning/src/StateAndProps.jsx
@@ -0,0 +1,10 @@
+function StateAndProps(p){
+    return(
+        <div>
+            <h1>
+                Your Current Name is : {p.name}
+            </h1>
+        </div>
+    )
+}
+export default StateAndProps;
