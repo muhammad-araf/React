a2210a9144828d205269a6d71ef48ea949511035
muhammad-araf <lentoaraf@gmail.com>
2025-07-06 12:12:07 +0500

    Props concept in React

--- Learning/src/App.js
+++ Learning/src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import ButtonEvent from './ButtonEvent';
 import StateIn from './StateIn';
 import CounterApp from './CounterApp';
+import PropsIn from './PropsIn';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Araf</h1>
-        <CounterApp />
+        {/* <h1>Araf</h1> */}
+        <PropsIn name={"Araf"}/>
 
 
 
---
+++ Learning/src/PropsIn.jsx
@@ -0,0 +1,8 @@
+function PropsIn(p){
+    return(
+        <div>
+            <h1>Hello {p.name}</h1>
+        </div>
+    )
+}
+export default PropsIn;
