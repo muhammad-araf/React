8d9fbb16c9a77316153cc9caf30e8c5457127722
muhammad-araf <lentoaraf@gmail.com>
2025-07-23 03:05:20 +0500

    Create context to edit my profile after register

--- src/Challenge01/DataProvider.js
+++ src/Challenge01/DataProvider.js
@@ -1,8 +1,7 @@
-// DataProvider.js
 import React, { createContext, useState } from 'react';
 
-// Create context
 export const DataContext = createContext();
+
 
 export const DataProvider = ({ children }) => {
   const [data, setData] = useState({});
